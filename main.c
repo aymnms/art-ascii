8a717014d59468885541f518084e42923f18e4b0
aymnms <aymerick.michelet@epsi.fr>
2019-04-14 19:31:29 +0200

    write sentence -> write (ABC)

--- main.c
+++ main.c
@@ -1,32 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void printA(){
-    printf(" ___\n");
-    printf("|   |\n");
-    printf("|___|\n");
-    printf("|   |\n");
-}
-
-void printB(){
-    printf(" ___\n");
-    printf("|   \\\n");
-    printf("|___/\n");
-    printf("|   \\\n");
-    printf("|___/\n");
-}
-
-void printC(){
-    printf(" ____\n");
-    printf("/\n");
-    printf("|\n");
-    printf("|\n");
-    printf("\\____\n");
-}
+char A[5][5] = { {' ', '_', '_', '_', ' '}, {'|', ' ', ' ', ' ', '|'}, {'|', '_', '_', '_', '|'}, {'|', ' ', ' ', ' ', '|'}, {'|', ' ', ' ', ' ', '|'} };
+char B[5][5] = { {' ', '_', '_', '_', ' '}, {'|', ' ', ' ', ' ', '\\'}, {'|', '_', '_', '_', '/'}, {'|', ' ', ' ', ' ', '\\'}, {'|', '_', '_', '_', '/'} };
+char C[5][5] = { {' ', '_', '_', '_', '_'}, {'/', ' ', ' ', ' ', ' '}, {'|', ' ', ' ', ' ', ' '}, {'|', ' ', ' ', ' ', ' '}, {'\\', '_', '_', '_', '_'} };
 
 int main(){
-    printA();
-    printB();
-    printC();
+    char rep[5];
+    int length;
+
+    printf("Hola, qu'est ce que tu veux écrire ? ");
+    scanf("%s", &rep);
+    length = strlen(rep);
+
+    for(int i = 0, imax = 5; i < imax; i++){
+        for(int lettre = 0; lettre < length; lettre++){
+            char temp = rep[lettre];
+            if (temp == 'A') {
+                for(int j = 0, jmax = 5; j < jmax; j++){
+                    printf("%c", A[i][j]);   
+                } 
+            }else if (temp == 'B') {
+                for(int j = 0, jmax = 5; j < jmax; j++){
+                    printf("%c", B[i][j]);   
+                }
+            }else if (temp == 'C') {
+                for(int j = 0, jmax = 5; j < jmax; j++){
+                    printf("%c", C[i][j]);   
+                }
+            }
+            printf(" ");
+        }
+        printf("\n");
+    }
+
     return 0;
 }
